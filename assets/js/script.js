c79e09b9ebaee0fa6f9d2a0dca48856f6f3c682b
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-04-16 21:59:44 -0500

    drag-drop completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,32 +184,60 @@ $(".list-group").on("blur", "input[type='text']", function() {
     })
     //END UPDATE TASK DATES
 
-//SORTABLE
-var tempArr = [];
+//SORTABLE (MOVING CARDS FROM ONE COLUMN TO ANOTHER)
 $(".card .list-group").sortable({
+    //enable dragging across lists
     connectWith: $(".card .list-group"),
     scroll: false,
     tolerance: "pointer",
     helper: "clone",
-    activate: function(event) {
-        console.log("activate", this);
+    activate: function(event, ui) {
+        console.log(ui);
     },
     over: function(event) {
-        console.log("out", event.target);
+        console.log(event);
+    },
+    out: function(event) {
+        console.log(event)
     },
     update: function(event) {
+        var tempArr = [];
+        //loop over current set of children in sortable list
         $(this).children().each(function() {
-            var text = $(this).find('p').text().trim();
-            var date = $(this).find('span').text().trim();
-            //add task data to the temp array as an object
-            tempArr.push({ text: text, date: date });
-            //trim down list's ID to match object property
-            var arrName = $(this).attr("id").replace("list-", "");
-            //update array on tasks object and save
-            tasks[arrName] = tempArr;
-            saveTasks();
-        })
+            //save values in a temp array
+            tempArr.push({
+                text: $(this).find("p").text().trim(),
+                date: $(this).find("span").text().trim()
+            });
+        });
+        //trim down list's ID to match object property
+        var arrName = $(this).attr("id").replace("list-", "");
+        //update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+    },
+    stop: function(event) {
+        $(this).removeClass("dropover");
     }
 });
+
+//DROP FOR DELETING CARDS
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+        console.log("drop");
+        ui.draggable.remove();
+    },
+    over: function(event, ui) {
+        console.log("over");
+    },
+    out: function(event, ui) {
+        console.log("out");
+    }
+});
+
+
+
 // load tasks for the first time
 loadTasks();
